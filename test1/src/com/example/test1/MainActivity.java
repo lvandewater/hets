7843001036eb87e81ac92e7818a84539bca3a65f
Luke Vandewater <luke.vandewater@outlook.com>
2014-03-11 07:34:09 +1100

    test3

--- test1/src/com/example/test1/MainActivity.java
+++ test1/src/com/example/test1/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 	
 	// here is a comment
+	// and another
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
